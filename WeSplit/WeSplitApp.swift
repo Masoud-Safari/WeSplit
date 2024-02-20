9c8e5961868a30dfb0a2038124aea6044e27d237
Masoud Safari <mmsafari@gmail.com>
2024-02-20 15:58:07 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Masoud Safari on 2024-02-20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
